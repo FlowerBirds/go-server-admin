5b503116949ea2daf45f8c44c40c85850722ca51
FlowerBirds <eabour@163.com>
2022-11-17 14:58:32 +0800

    增加获取exe当前路径方法

--- util/common_util.go
+++ util/common_util.go
@@ -2,9 +2,14 @@ package util
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"time"
 )
 
@@ -36,4 +41,23 @@ func ResponseWithJson(v interface{}, w http.ResponseWriter) {
 
 func IsInstanceOf(o1, o2 interface{}) bool {
 	return reflect.TypeOf(o1) == reflect.TypeOf(o2)
+}
+
+func GetCurrentPath() (string, error) {
+	file, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		return "", err
+	}
+	path, err := filepath.Abs(file)
+	if err != nil {
+		return "", err
+	}
+	i := strings.LastIndex(path, "/")
+	if i < 0 {
+		i = strings.LastIndex(path, "\\")
+	}
+	if i < 0 {
+		return "", errors.New(`error: Can't find "/" or "\".`)
+	}
+	return string(path[0 : i+1]), nil
 }
